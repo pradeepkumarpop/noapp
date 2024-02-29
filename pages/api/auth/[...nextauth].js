b6991727a75bc085f5c7a6353f9da240ad05b4cb
pradeep.kumar <pradeep.kumar@mastersoftwaresolutions.com>
2024-02-29 13:00:54 +0530

    authentication with google and github in next 11

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,37 @@
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
+
+export default NextAuth({
+  providers: [
+    Providers.Google({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      authorizationUrl:
+        'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code',
+    }),
+    Providers.GitHub({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+      redirectUri: 'http://localhost:3000/api/auth/callback/github',
+      // scope: ['repo', 'pradeepkumarpop:mss.pradeepkumar@gmail.com'],
+    }),
+  ],
+  jwt: {
+    encryption: true,
+  },
+  secret: process.env.SECRET,
+  callbacks: {
+    async jwt(token, account) {
+      if (account?.accessToken) {
+        token.accessToken = account.accessToken;
+      }
+      return token;
+    },
+    redirect: async (url, _baseUrl) => {
+      if (url === '/profile') {
+        return Promise.resolve('/');
+      }
+      return Promise.resolve('/');
+    },
+  },
+});
---
+++ pages/index.js
@@ -0,0 +1,61 @@
+import Head from 'next/head';
+import { signIn, signOut, useSession } from 'next-auth/client';
+import styles from '../styles/Home.module.css';
+import Router from 'next/router'
+
+export default function Home() {
+  const [session, loadingSession] = useSession();
+
+
+  console.log(session)
+
+  if (loadingSession) {
+    return <p>Loading...</p>;
+  }
+  const dashBoard = () => {
+    Router.push('/homescreen')
+  }
+  return (
+    <div className={styles.container}>
+
+      <h1>Hi  there! Welcome to our website Technical Rajni </h1>
+
+      {!session && (
+        <>
+          <button className={styles.primaryButton} onClick={() => signIn("google")}>
+            Sign In
+          </button >
+          <button className={styles.primaryButton} onClick={() => signIn("github")}>
+            Sign in with GitHub
+          </button>
+        </>
+      )}
+
+      {session && (
+        <>
+          <h4>You are logged as: {session.user.name}</h4>
+          <div className={styles.boxCenter}>
+            <h4>Email: {session.user.email} </h4>
+            <br />
+            {session.user.image && (
+              <span>
+                <img src={String(session.user.image)} alt={session.user.name} width={100} height={80} />
+              </span>
+            )}
+          </div>
+          <br />
+          <br />
+          <button className={styles.primaryButton} onClick={() => signOut()}>
+            Sign Out
+          </button>
+          <br />
+          <br />
+          <button className={styles.primaryButton} onClick={() => dashBoard()}>
+            Go to dashBoard
+          </button>
+
+        </>
+      )}
+    </div>
+  );
+}
